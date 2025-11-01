c4b62831ef8ea6144eed4801ecb1c4ea5bec8d4b
kishore-sv <kishorbunni7@gmail.com>
2025-11-01 11:52:36 +0530

    init

---
+++ app/src/main/java/com/example/interview_app/login.java
@@ -0,0 +1,50 @@
+package com.example.interview_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView signupTv=findViewById(R.id.signupText);
+        Button loginBtn=findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(login.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+        signupTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(login.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
